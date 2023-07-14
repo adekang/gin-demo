86f3c8f83968ddaccaeb04e987dba6ed381b9d9a
adekang <adekang@163.com>
2023-07-14 20:53:08 +0800

    feat: 登录接口

--- controller/UserController.go
+++ controller/UserController.go
@@ -8,20 +8,75 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
+func Login(c *gin.Context) {
+	DB := common.GetDB()
+	// 获取参数
+	password := c.PostForm("password")
+	telephone := c.PostForm("telephone")
+
+	// 数据验证
+	if len(telephone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "手机号码不正确,必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "密码不能少于6位",
+		})
+		return
+	}
+	// 判断手机号是否存在
+	var user model.User
+	DB.Where("telephone=?", telephone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "用户不存在",
+		})
+		return
+	}
+	// 判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": 400,
+			"msg":  "密码错误",
+		})
+		return
+	}
+
+	// 发放token
+	token := "11111111"
+
+	// 返回结果
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登录成功",
+	})
+
+}
+
 func Register(c *gin.Context) {
 	DB := common.GetDB()
+
+	// 获取参数
 	name := c.PostForm("name")
 	password := c.PostForm("password")
 	telephone := c.PostForm("telephone")
+
+	// 数据验证
 	c.JSON(http.StatusOK, gin.H{
 		"username": name,
 		"password": password,
 	})
-
-	log.Println(telephone, len(telephone))
 
 	if len(telephone) != 11 {
 		c.JSON(http.StatusUnprocessableEntity, gin.H{
@@ -44,7 +99,6 @@ func Register(c *gin.Context) {
 
 	log.Println(name, password, telephone)
 	// 判断手机号是否存在
-
 	if isTelExist(DB, telephone) {
 		c.JSON(http.StatusUnprocessableEntity, gin.H{
 			"code": 422,
@@ -52,15 +106,26 @@ func Register(c *gin.Context) {
 		})
 	}
 
+	// 密码加密
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": 500,
+			"msg":  "加密错误",
+		})
+		return
+	}
+
 	// 创建用户
 	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasedPassword),
 	}
 
 	DB.Create(&newUser)
 
+	// 返回结果
 	c.JSON(http.StatusOK, gin.H{
 		"msg": "注册成功",
 	})
